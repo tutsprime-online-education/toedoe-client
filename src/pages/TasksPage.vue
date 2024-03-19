493b217522f4a08ef7aceb05047e997405da3cdc
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 05:21:01 +0700

    Setup axios HTTP client

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -75,3 +75,12 @@
         </div>
     </main>
 </template>
+<script setup>
+import { onMounted } from "vue";
+import { allTasks } from "../http/task-api";
+
+onMounted(async () => {
+    const { data } = await allTasks();
+    console.log(data);
+});
+</script>
