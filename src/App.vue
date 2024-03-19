9934becfb4fd872ec0884acc73fcf0d0cf3b85d6
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 05:12:10 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import TasksPage from "./pages/TasksPage.vue";
+</script>
+
+<template>
+    <Navbar />
+    <TasksPage />
+    <Footer />
+</template>
+
+<style scoped></style>
