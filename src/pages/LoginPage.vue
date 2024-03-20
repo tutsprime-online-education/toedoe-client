2f14115beea3dca49a986870e757a10b324c7510
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:42:07 +0700

    Use the useRoute function for programmatic navigation

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Login</h1>
+    <button class="btn btn-primary" @click="handleClick">Login</button>
 </template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleClick = () => {
+    router.push({ name: "tasks" });
+};
+</script>
