58c744dcbcf797dec4928c61ac99ed708b90a9f8
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:46:36 +0700

    Handle unavailable routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import TasksPage from "../pages/TasksPage.vue";
 import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
 import SummaryPage from "../pages/SummaryPage.vue";
+import NotFoundErrorPage from "../pages/errors/NotFoundErrorPage.vue";
 
 const routes = [
   {
@@ -23,6 +24,11 @@ const routes = [
     path: "/register",
     component: RegisterPage,
     name: "register",
+  },
+  {
+    path: "/:notFound(.*)",
+    name: "error.404",
+    component: NotFoundErrorPage,
   },
 ];
 
