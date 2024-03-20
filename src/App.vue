0f5b31e48bc4bfb0b54283be1fda621be959082e
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:37:55 +0700

    Setup vue router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
-import TasksPage from "./pages/TasksPage.vue";
 </script>
 
 <template>
     <Navbar />
-    <TasksPage />
+    <router-view />
     <Footer />
 </template>
 
