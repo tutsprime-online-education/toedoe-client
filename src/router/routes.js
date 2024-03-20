d82699f55d0fa39a77843a866b2c858c6fa75d60
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 12:41:37 +0700

    Create pinia store for authentication

--- src/router/routes.js
+++ src/router/routes.js
@@ -15,17 +15,17 @@ const routes = [
     path: "/tasks",
     component: TasksPage,
     name: "tasks",
-    // meta: {
-    //   auth: true,
-    // },
+    meta: {
+      auth: true,
+    },
   },
   {
     path: "/summary",
     component: SummaryPage,
     name: "summary",
-    // meta: {
-    //   auth: true,
-    // },
+    meta: {
+      auth: true,
+    },
   },
   {
     path: "/login",
