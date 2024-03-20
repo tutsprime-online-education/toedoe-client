1d7ef5a68a128a107420c3ba51fcca6670d0d055
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:44:18 +0700

    Setup the link active class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -22,9 +22,12 @@
                         <router-link :to="{ name: 'tasks' }" class="nav-link"
                             >Tasks</router-link
                         >
+                        <!-- <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Tasks</a> -->
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Summary</a>
+                        <router-link :to="{ name: 'summary' }" class="nav-link"
+                            >Summary</router-link
+                        >
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
@@ -52,3 +55,9 @@
         </div>
     </nav>
 </template>
+
+<style scoped>
+.nav-link.router-link-active {
+    color: rgba(0, 0, 0, 0.9);
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,18 @@
 import TasksPage from "../pages/TasksPage.vue";
 import LoginPage from "../pages/LoginPage.vue";
 import RegisterPage from "../pages/RegisterPage.vue";
+import SummaryPage from "../pages/SummaryPage.vue";
 
 const routes = [
   {
     path: "/tasks",
     component: TasksPage,
     name: "tasks",
+  },
+  {
+    path: "/summary",
+    component: SummaryPage,
+    name: "summary",
   },
   {
     path: "/login",
