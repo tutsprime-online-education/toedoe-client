e20a6e2da6d0ece317e9bf9b2fdb89d362a8040f
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:49:15 +0700

    Use the route meta fields

--- src/router/index.js
+++ src/router/index.js
@@ -8,8 +8,13 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from) => {
-  if (to.path === "/tasks") {
-    return { name: "login" };
+  if (to.meta.auth) {
+    return {
+      name: "login",
+      query: {
+        redirect: to.fullPath,
+      },
+    };
   }
 });
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -9,11 +9,17 @@ const routes = [
     path: "/tasks",
     component: TasksPage,
     name: "tasks",
+    meta: {
+      auth: true,
+    },
   },
   {
     path: "/summary",
     component: SummaryPage,
     name: "summary",
+    meta: {
+      auth: true,
+    },
   },
   {
     path: "/login",
