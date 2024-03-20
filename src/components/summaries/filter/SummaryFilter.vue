4cc9db508ed637310128c76f32d494afb4032b5b
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 17:47:00 +0700

    Add additional options to the Summary filter component

--- src/components/summaries/filter/SummaryFilter.vue
+++ src/components/summaries/filter/SummaryFilter.vue
@@ -1,7 +1,25 @@
 <script setup>
+import { computed } from "vue";
+import { startOfWeek, endOfWeek, subWeeks, format } from "date-fns";
 import Dropdown from "../../dropdown/Dropdown.vue";
 import DropdownItem from "../../dropdown/DropdownItem.vue";
 import DropdownTrigger from "../../dropdown/DropdownTrigger.vue";
+
+const filterItems = computed(() => {
+    const thisWeekStart = format(startOfWeek(new Date()), "d MMM");
+    const thisWeekEnd = format(endOfWeek(new Date()), "d MMM");
+    const lastWeekStart = format(startOfWeek(subWeeks(new Date(), 1)), "d MMM");
+    const lastWeekEnd = format(endOfWeek(subWeeks(new Date(), 1)), "d MMM");
+
+    return {
+        today: "Today",
+        yesterday: "Yesterday",
+        thisweek: `This week (${thisWeekStart} - ${thisWeekEnd})`,
+        lastweek: `Last week (${lastWeekStart} - ${lastWeekEnd})`,
+        thismonth: "This month",
+        lastmonth: "Last month",
+    };
+});
 </script>
 
 <template>
@@ -16,9 +34,12 @@ import DropdownTrigger from "../../dropdown/DropdownTrigger.vue";
             >
         </template>
         <template v-slot:menu="{ toggle }">
-            <DropdownItem href="#" @click.prevent="toggle">Today</DropdownItem>
-            <DropdownItem href="#" @click.prevent="toggle"
-                >Yesterday</DropdownItem
+            <DropdownItem
+                v-for="(value, key) in filterItems"
+                :key="key"
+                href="#"
+                @click.prevent="toggle"
+                >{{ value }}</DropdownItem
             >
         </template>
     </Dropdown>
