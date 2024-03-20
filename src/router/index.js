465149b296ba6587c5790775074ca8ba8e371cf3
Eding Muhamad Saprudin <masaruedo@yahoo.com>
2024-03-20 08:47:54 +0700

    Use global before each guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 
-export default createRouter({
+const router = createRouter({
   routes,
   history: createWebHistory(),
   // linkActiveClass: "active",
 });
+
+router.beforeEach((to, from) => {
+  if (to.path === "/tasks") {
+    return { name: "login" };
+  }
+});
+
+export default router;
